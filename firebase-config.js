6332bf13c826b0e8263ec4665248811ac6cb4ef6
David <87504880+darud63@users.noreply.github.com>
2025-01-03 23:48:50 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id"
+  apiKey: "AIzaSyAERuRbz1wE_b34wZgmb_nbNMGF4RpPaF4",
+  authDomain: "eten-373bf.firebaseapp.com",
+  databaseURL: "https://eten-373bf-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "eten-373bf",
+  storageBucket: "eten-373bf.firebasestorage.app",
+  messagingSenderId: "21158034114",
+  appId: "1:21158034114:web:0cc51995fcef064f19595b",
+  measurementId: "G-5MMWNKP6PB"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Инициализация Firebase
 firebase.initializeApp(firebaseConfig);
