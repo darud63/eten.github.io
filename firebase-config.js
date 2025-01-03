fc834c47c00d74f27c9635e9648f2369647f1999
David <87504880+darud63@users.noreply.github.com>
2025-01-03 23:54:16 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAERuRbz1wE_b34wZgmb_nbNMGF4RpPaF4",
   authDomain: "eten-373bf.firebaseapp.com",
   databaseURL: "https://eten-373bf-default-rtdb.europe-west1.firebasedatabase.app",
